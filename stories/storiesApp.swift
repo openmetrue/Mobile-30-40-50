1b3218b18c6b1798251ebee910ef0559b954b4e1
openmetrue <openmetrue@gmail.com>
2022-06-05 01:40:30 +0800

    Initial Commit

---
+++ stories/storiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  storiesApp.swift
+//  stories
+//
+//  Created by Mark Khmelnitskii on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct storiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
